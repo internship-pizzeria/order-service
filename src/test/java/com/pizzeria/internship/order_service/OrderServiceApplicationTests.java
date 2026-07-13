9037766785bce6ec1c41053c17a26abcbb0e193f
Igor Wieczorek <igor.wieczorek@MAC-D7353996R1.local>
2026-07-13 10:50:44 +0200

    Inicjalizacja projektu spring boot dla order service

---
+++ src/test/java/com/pizzeria/internship/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pizzeria.internship.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
