c64162de011e582e497040be992ccfc98a8cc643
igor.wieczorek <i.wieczorek213@Gmail.com>
2026-07-23 12:34:00 +0200

    Changed the scheduler to use Shedlock to prevent too many requests on the db

--- src/main/java/com/pizzeria/internship/order_service/OrderServiceApplication.java
+++ src/main/java/com/pizzeria/internship/order_service/OrderServiceApplication.java
@@ -1,17 +1,34 @@
 package com.pizzeria.internship.order_service;
 
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableRetry
 @EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "10m")
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+	@Bean
+	public LockProvider lockProvider(DataSource dataSource) {
+		return new JdbcTemplateLockProvider(
+				JdbcTemplateLockProvider.Configuration.builder()
+						.withJdbcTemplate(new org.springframework.jdbc.core.JdbcTemplate(dataSource))
+						.usingDbTime()
+						.build()
+		);
 	}
 
 }
