b5bbf152cf7ca5cfbc722d020615804051aaba0c
Hubert Jaskuła <hubertjaskula516@gmail.com>
2026-07-28 15:37:11 +0200

    fix: correct analytics datasource configuration to not conflict with primary JPA

--- src/main/java/com/pizzeria/internship/order_service/config/AnalyticsDataSourceConfig.java
+++ src/main/java/com/pizzeria/internship/order_service/config/AnalyticsDataSourceConfig.java
@@ -4,7 +4,12 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
 
 @Configuration
 class AnalyticsDataSourceConfig {
@@ -14,10 +19,31 @@ class AnalyticsDataSourceConfig {
             @Value("${analytics.datasource.url}") String url,
             @Value("${analytics.datasource.username}") String username,
             @Value("${analytics.datasource.password}") String password) {
+        HikariDataSource ds = createAnalyticsDataSource(url, username, password);
+        return new JdbcTemplate(ds);
+    }
+
+    @Bean
+    DataSourceInitializer analyticsDataSourceInitializer(
+            @Value("${analytics.datasource.url}") String url,
+            @Value("${analytics.datasource.username}") String username,
+            @Value("${analytics.datasource.password}") String password) {
+        HikariDataSource ds = createAnalyticsDataSource(url, username, password);
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("analytics-schema.sql"));
+        populator.setSeparator(";");
+        DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(ds);
+        initializer.setDatabasePopulator(populator);
+        return initializer;
+    }
+
+    private HikariDataSource createAnalyticsDataSource(String url, String username, String password) {
         HikariDataSource ds = new HikariDataSource();
         ds.setJdbcUrl(url);
         ds.setUsername(username);
         ds.setPassword(password);
-        return new JdbcTemplate(ds);
+        ds.setPoolName("analytics-hikari-pool");
+        return ds;
     }
 }
